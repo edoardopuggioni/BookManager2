c18016622fe94ca9e1c505d5ec00c5cf6ef9f0b7
Edoardo Puggioni <puggioniedoardo@gmail.com>
2018-09-28 16:59:06 +0200

    I only copied Java classes from Lab2, except from DetailActivity and SummaryActivity, which we probably have to change because we have to deal with adaptors and fragments.

---
+++ app/src/main/java/com/example/android/bookmanager2/BookManagerInterface.java
@@ -0,0 +1,28 @@
+package com.example.android.bookmanager2;
+
+import java.util.ArrayList;
+
+public interface BookManagerInterface
+{
+    public int count();
+
+    public Book getBook(int index);
+
+    public Book createBook();
+
+    public ArrayList<Book> getAllBooks();
+
+    public void removeBook(Book book);
+
+    public void moveBook(int from, int to);
+
+    public int getMinPrice();
+
+    public int getMaxPrice();
+
+    public float getMeanPrice();
+
+    public int getTotalCost();
+
+    public void saveChanges();
+}
---
+++ app/src/main/java/com/example/android/bookmanager2/SimpleBookManager.java
@@ -0,0 +1,112 @@
+package com.example.android.bookmanager2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SimpleBookManager implements BookManagerInterface
+{
+    private ArrayList<Book> bookList = new ArrayList<>();
+
+    public SimpleBookManager()
+    {
+        createBook("Bill Bryson","A Short History of Nearly Everything ",145,"0-7679-0817-1","Life" );
+        createBook("Elizabeth Kostova","The Historian",99,"0-316-01177-0","English" );
+        createBook("Andrew S. Tanenbaum","Modern Operating Systems",120," 0132199084","Operating systems" );
+        createBook("Bjarne Stroustrup","The C++ Programming Language",999,"978-0321563842","OOP" );
+        createBook("Collins Gem","SAS Survival Guide Handbook",399," 0060849827","Life" );
+    }
+
+    @Override
+    public int count() {
+        return bookList.size();
+    }
+
+    @Override
+    public Book getBook(int index) {
+        return bookList.get(index);
+    }
+
+    @Override
+    public Book createBook()
+    {
+        Book InstanceOfBook = new Book();
+        bookList.add(InstanceOfBook);
+        return InstanceOfBook;
+    }
+    //this is a overloading method to create books with data
+
+    public Book createBook(String author, String title, int price, String isbn, String course)
+    {
+        Book InstanceOfBook = new Book( author, title, price, isbn, course );
+        bookList.add(InstanceOfBook);
+        return InstanceOfBook;
+    }
+
+    @Override
+    public ArrayList<Book> getAllBooks() {
+        return bookList;
+    }
+
+    @Override
+    public void removeBook(Book book) {
+        bookList.remove(book);
+    }
+
+    @Override
+    public void moveBook(int from, int to) {
+        Collections.swap(bookList, from, to);
+    }
+
+    @Override
+    public int getMinPrice()
+    {
+        int minValue = 0;
+        for (int i = 0; i < bookList.size(); i++) {
+            Book book = (bookList.get(i));
+            if (i == 0) minValue = book.getPrice();
+            if (book.getPrice() < minValue) minValue = book.getPrice();
+        }
+        return minValue;
+    }
+
+    @Override
+    public int getMaxPrice()
+    {
+        int maxValue = 0;
+        for (int i = 0; i < bookList.size(); i++) {
+            Book book = (bookList.get(i));
+            if (book.getPrice() > maxValue) maxValue = book.getPrice();
+        }
+        return maxValue;
+    }
+
+    @Override
+    public float getMeanPrice()
+    {
+        float sum = 0;
+
+        for (int i = 0; i < bookList.size(); i++) {
+            Book book = (bookList.get(i));
+            sum = sum + book.getPrice();
+        }
+
+        return sum / bookList.size();
+    }
+
+    @Override
+    public int getTotalCost()
+    {
+        int sum = 0;
+        for (int i = 0; i < bookList.size(); i++) {
+            Book book = (bookList.get(i));
+            sum = sum + book.getPrice();
+        }
+        return sum;
+    }
+
+    @Override
+    public void saveChanges()
+    {
+        // empty method
+    }
+}
