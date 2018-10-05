386cba9d187e4c5bbe9f46ddf3a55284b738cd48
Andreas Koj <kojandreas@gmail.com>
2018-10-05 21:47:08 +0200

    - added new activity BookEdit

--- app/src/main/java/com/example/android/bookmanager2/BookDetails.java
+++ app/src/main/java/com/example/android/bookmanager2/BookDetails.java
@@ -1,55 +1,17 @@
 package com.example.android.bookmanager2;
 
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
 
 public class BookDetails extends AppCompatActivity {
-
-    String BookNumber;
-    SimpleBookManager sbm = SimpleBookManager.getBookManager();
-
-    EditText etAuthor;
-    EditText etTitle;
-    EditText etCourse;
-    EditText etISBN ;
-    EditText etPrice;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_details);
-        Intent CaughtIntent = getIntent();
-        BookNumber = CaughtIntent.getStringExtra(EXTRA_MESSAGE);
-
-        // fill edit boxes with data
-
-        etAuthor = (EditText) findViewById(R.id.etAuthor);
-        etTitle = (EditText) findViewById(R.id.etTitle);
-        etCourse = (EditText) findViewById(R.id.etCourse);
-        etPrice = (EditText) findViewById(R.id.etPrice);
-        etISBN = (EditText) findViewById(R.id.etISBN);
-
-        etAuthor.setText(sbm.getBook(Integer.parseInt(BookNumber)).getAuthor());
-        etTitle.setText(sbm.getBook(Integer.parseInt(BookNumber)).getTitle());
-        etCourse.setText(sbm.getBook(Integer.parseInt(BookNumber)).getCourse());
-        etISBN.setText(sbm.getBook(Integer.parseInt(BookNumber)).getIsbn());
-        etPrice.setText("" + sbm.getBook(Integer.parseInt(BookNumber)).getPrice());
-    }
+            }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -57,40 +19,6 @@ public class BookDetails extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_details, menu);
         return super.onCreateOptionsMenu(menu);
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.action_addBook:
-
-                //number of the book to replace
-                sbm.getBook(Integer.parseInt(BookNumber));
-
-
-                Book book = new Book(etAuthor.getText().toString(),
-                        etTitle.getText().toString(),
-                        Integer.parseInt( etPrice.getText().toString()),
-                        etISBN.getText().toString(),
-                        etCourse.getText().toString()
-                        );
-
-
-                Intent intentFromAdd = new Intent(BookDetails.this, MainActivity.class);
-                startActivity(intentFromAdd);
-                return true;
-
-            case R.id.action_deleteBook:
-
-                sbm.removeBook(sbm.getBook(Integer.parseInt(BookNumber)));
-                Context context = getApplicationContext();
-                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-                sbm.saveChanges(prefs);
-                Intent intentFromDel = new Intent(BookDetails.this, MainActivity.class);
-                startActivity(intentFromDel);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/example/android/bookmanager2/CollectionTabFragment.java
+++ app/src/main/java/com/example/android/bookmanager2/CollectionTabFragment.java
@@ -3,17 +3,13 @@ package com.example.android.bookmanager2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
