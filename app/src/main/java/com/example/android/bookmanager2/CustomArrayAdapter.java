d2f1654e298da16d1ab7652204f0cd04211046c1
Andreas Koj <kojandreas@gmail.com>
2018-10-18 19:50:14 +0200

    ArrayAdapter is finally working. Some visual changes in challenge activity + changed colors in whole app

--- app/src/main/java/com/example/android/bookmanager2/CustomArrayAdapter.java
+++ app/src/main/java/com/example/android/bookmanager2/CustomArrayAdapter.java
@@ -1,14 +1,12 @@
 package com.example.android.bookmanager2;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 import java.util.List;
 
